b2db5883e009265b71b725434748576ae745c63e
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2025-09-21 11:13:02 -0300

    Update FreeplayStateNOVA.hx

--- source/extras/states/FreeplayStateNOVA.hx
+++ source/extras/states/FreeplayStateNOVA.hx
@@ -151,7 +151,6 @@ class FreeplayStateNOVA extends HScriptStateHandler
 			return Reflect.compare(a.songName.toLowerCase(), b.songName.toLowerCase());
 		});
 
-		Mods.loadTopMod();
 
 		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
 		magenta.scale.x = FlxG.width * 1.05 / magenta.width;
@@ -401,7 +400,6 @@ class FreeplayStateNOVA extends HScriptStateHandler
 
 	var pressCheck:Bool = false;
 	function backMenu() {
-		Mods.loadTopMod();
 		if (!pressCheck){
 			pressCheck = true;
 			FlxG.sound.music.stop();
